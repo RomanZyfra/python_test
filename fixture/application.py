67c11e8d7f38cfc85bcccbd223552cdda6df91bc
RomanQA110591 <roman.sapachev@mail.ru>
2022-01-02 16:55:27 +0300

    DZ_5

---
+++ fixture/application.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+from fixture.contacts import ContactsHelper
+
+
+class Application:
+    def __init__(self):
+        self.wd = webdriver.Chrome('C:\\chromedriver\\chromedriver.exe')
+        self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
+        self.contacts = ContactsHelper(self)
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/addressbook/")
+
+    def destroy(self):
+        self.wd.quit()
---
+++ fixture/contacts.py
@@ -0,0 +1,43 @@
+class ContactsHelper:
+    def __init__(self, app):
+        self.app = app
+
+    def create_contacts(self, contacts):
+        wd = self.app.wd
+        # init contacts creation
+        wd.find_element_by_link_text("add new").click()
+        # fill contacts form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contacts.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contacts.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contacts.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contacts.nickname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contacts.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contacts.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contacts.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contacts.home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contacts.mobile)
+        # submit contacts creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_home_page()
+
+    def return_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
