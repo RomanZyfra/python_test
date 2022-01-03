0562535e9ef1edb3d69e38d3c97eaa765b38a0bd
RomanQA110591 <roman.sapachev@mail.ru>
2022-01-03 19:24:50 +0300

    DZ_8_переименование

--- test/test_add_contacts.py
+++
@@ -1,8 +0,0 @@
-from model.contacts import Contacts
-
-
-def test_add_contacts(app):
-    app.session.login(username="admin", password="secret")
-    app.contacts.create(Contacts(firstname="Test", middlename="Test", lastname="test", nickname="test",
-                                 title="test", company="test", address="test", home="55555", mobile="777777"))
-    app.session.logout()
--- test/test_del_contacts.py
+++
@@ -1,4 +0,0 @@
-def test_delete_first_contacts(app):
-    app.session.login(username="admin", password="secret")
-    app.contacts.delete_first_contacts()
-    app.session.logout()
